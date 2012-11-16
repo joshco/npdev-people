9eb92c6c8f8539134d0330d36fdd181bd148f5b7
Josh Cohen <joshco@gmail.com>
2012-11-16 14:45:14 -0800

    initial checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+People::Application.config.secret_token = '154ed3cf1799c98b1dd0b3e733fc926132b76f13d389e029eb6426d329c6278d758446274aef56f0efcbe81b781f71eb786512d4345ac07cdd0a63d8a2275225'
